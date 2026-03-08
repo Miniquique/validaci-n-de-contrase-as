d37e8f3d902fd2d33a2994d9cd4e8add6a1455e2
David <122730856+Miniquique@users.noreply.github.com>
2026-03-08 20:56:17 +0100

    Update validacionDeContraseñas.java

--- validacionDeContraseñas.java
+++ validacionDeContraseñas.java
@@ -6,35 +6,51 @@ public class validacionDeContraseñas {
 
 	public static void main(String[] args) {
 		var sc = new Scanner(System.in);
-		
-		String contraseña = sc.nextLine();
-		int tamaño = contraseña.length();
+		boolean valido = false;
 		String especiales = "@#$%&";
 		boolean tamañoC = true;
- 		boolean mayuscula = false;
+		boolean mayuscula = false;
 		boolean caracterS = false;
 		boolean numero = false;
-		
-		if (!contraseña.equals(contraseña.toLowerCase())){
-			mayuscula = true;
-		}
-		if (tamaño<8||tamaño>20)tamañoC = false;
-		
-		for (int i=0;i<=tamaño-1;i++) {
-			char caracter= contraseña.charAt(i);
-			String car= String.valueOf(caracter);
-			switch(caracter){
-				case '1','2','3','4','5','6','7','8','9','0'-> numero=true; 
+		String contraseña = "";
+
+
+		do {
+			//se necesita para que no se borren las variables
+			tamañoC = true;
+			mayuscula = false;
+			caracterS = false;
+			numero = false;
+			
+			System.out.println("introduce la contraseña");
+			contraseña = sc.nextLine();
+			int tamaño = contraseña.length();
+			if (!contraseña.equals(contraseña.toLowerCase())) {
+				mayuscula = true;
 			}
-			if(especiales.contains(car)) {
-				caracterS=true;
-			}
-			if(caracterS&&numero&&mayuscula)break;
+			if (tamaño < 8 || tamaño > 20)
+				tamañoC = false;
+
+			for (int i = 0; i <= tamaño - 1; i++) {
+				char caracter = contraseña.charAt(i);
+				String car = String.valueOf(caracter);
 				
-		}
-		if(caracterS&&numero&&mayuscula&&tamañoC) {
-			System.out.println("contraseña registrada correctamente");
-		}else System.out.println("la contraseña debe cumplir los requisitos de seguridad");
+				if (Character.isDigit(caracter)) {
+			        numero = true;
+			    }
+				if (especiales.contains(car)) {
+					caracterS = true;
+				}
+				if (caracterS && numero && mayuscula)
+					break;
+
+			}
+			if (caracterS && numero && mayuscula && tamañoC) {
+				System.out.println("contraseña registrada correctamente");
+				valido = true;
+			} else
+				System.out.println("la contraseña debe cumplir los requisitos de seguridad");
+		} while (!valido);
 		sc.close();
 	}
 
